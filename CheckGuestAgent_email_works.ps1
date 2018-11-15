f99ca090206edacc8e51bef1af9bb4bd93257dc9
Michael Recinto <35378940+recintoit@users.noreply.github.com>
2018-11-15 07:46:26 -0800

    Add files via upload

---
+++ CheckGuestAgent_email_works.ps1
@@ -0,0 +1,26 @@
+﻿#variables used
+#There are a few servers, so I hardcoded. But best practice would be to get-content from a server.txt file
+
+$Servers = "<serverNames>"
+ 
+ foreach ($Server in $Servers) 
+
+ #MS changed the filename, so this would need to be updated
+ {Foreach-Object  ([XML]$Agent = Get-Content \\$Server\c$\windowsazure\GuestAgent_2.7.41491.895*\CommonAgentConfig.config)
+ $agent.configuration.'microsoft.windowsAzure.guestAgent.configuration'.environment.enablePushInVMLogs 
+
+if  ($agent.configuration.'microsoft.windowsAzure.guestAgent.configuration'.environment.enablePushInVMLogs -eq "false")  { write-host "yes"} 
+else {Send-MailMessage -From  -To  -Subject "AzureGuestAgent set to True" -SmtpServer  -Body "There is no Azure Guest Agent config on $Server "  }
+
+
+
+   }
+   
+   
+   
+   
+   
+   
+   
+
+  
